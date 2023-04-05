7269fd6fd287b8e273be49d90fd8703e4e7e4cce
Adam Washington <adam.washington@stfc.ac.uk>
2023-04-05 14:23:09 +0100

    Add comments to first main.py

--- hello/main.py
+++ hello/main.py
@@ -2,10 +2,14 @@ from PySide2.QtWidgets import QApplication
 from PySide2.QtQuick import QQuickView
 from PySide2.QtCore import QUrl
 
+# Create an appliction to process all of the events
 app = QApplication([])
+# Create a window to display the QML file
 view = QQuickView()
-url = QUrl("view.qml")
 
-view.setSource(url)
+# Point the window at the QML we created
+view.setSource("view.qml")
+# Open the window
 view.show()
+# Run the application
 app.exec_()
